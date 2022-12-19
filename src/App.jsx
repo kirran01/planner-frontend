c0b027d4bd914fd9761ec37ecf378afd6d43c3e9
kirran01 <Kirran-@live.com>
2022-12-19 15:55:51 -0500

    prfile page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/home'
 import DaySelect from './components/dayselect'
 import Login from './pages/login'
 import Signup from './pages/signup'
+import Profile from './pages/profile'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
+      <Route path='/profile' element={<Profile/>}/>
       </Routes>  
     </div>
   )
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
 
 const Profile = () => {
+    const{user,isLoggedIn,logOut}=useContext(AuthContext)
     return (
         <div>
-            <h1>user profile</h1>
+            {
+                user&&(
+                    <>
+                    <h1 style={{textAlign:'center'}}>Profile</h1>
+                    <h3>username: {user.name}</h3>
+                    <h3>email:{user.email}</h3>
+                    </>
+                )
+            }
+            {
+                !user&&(
+                    <>
+                    <h4>loading...</h4>
+                    </>
+                )
+            }
         </div>
     );
 }
