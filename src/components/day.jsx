4ae8b1ad2dc34ca358ee5698b44b1ced02b48d9b
kirran01 <Kirran-@live.com>
2022-12-22 23:45:55 -0500

    changing date in front end

--- src/components/day.jsx
+++ src/components/day.jsx
@@ -5,90 +5,102 @@ import Evententry from './evententry';
 
 const Day = (props) => {
   // {import.meta.env.VITE_BACKEND_URL}
-  const[dateTag,setDateTag]=useState(props.dayObj.day)
-  const[isEditing,setIsEditing]=useState(false)
-  const[showChange,setShowChange]=useState(false)
+  const [dateTag, setDateTag] = useState(props.dayObj.day)
+  const [isEditing, setIsEditing] = useState(false)
+  const [showChange, setShowChange] = useState(false)
 
-  const handleDayInputState=(e)=>{
-  e.preventDefault()
-  console.log(e.target.value)
-  setDateTag(e.target.value + 'T00:00:00')
+  const handleDayInputState = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    setDateTag(e.target.value + 'T00:00:00')
   }
 
-  const editDate=()=>{
-    if(!isEditing){
+  const editDate = () => {
+    if (!isEditing) {
       setIsEditing(true)
-    }else{
+    } else {
       setIsEditing(false)
     }
   }
 
-  const submitEditedDate=(e)=>{
-  // e.preventDefault()
-  //dateTag is not set here. 
-  axios.put(`${import.meta.env.VITE_BACKEND_URL}/days/all/${props.dayObj._id}`,{
-    day:dateTag
-  })
-  .then(updatedDay=>{
-    setDateTag(updatedDay.data.day)
-    setIsEditing(false)
-  })
-  .catch(err=>{
-    console.log(err)
-  })
+  const submitEditedDate = (e) => {
+    // e.preventDefault()
+    //dateTag is not set here. 
+    axios.put(`${import.meta.env.VITE_BACKEND_URL}/days/all/${props.dayObj._id}`, {
+      day: dateTag
+    })
+      .then(updatedDay => {
+        console.log('updatedDay', updatedDay)
+        const dayId = updatedDay.data._id;
+        // Update that particular day in the allDays array
+        const allDays = props.allDays.map(day => {
+          if (day._id === dayId) {
+            return { ...day, day: dateTag };
+          }
+          return day;
+        });
+        props.setAllDays(allDays);
+        console.log('dateTag', dateTag);
+        setDateTag(updatedDay.data.day)
+        setIsEditing(false)
+      })
+      .catch(err => {
+        console.log(err)
+      })
 
   }
 
-  const removeDay=(e)=>{
-  axios.delete(`${import.meta.env.VITE_BACKEND_URL}/days/all/${props.dayObj._id}`)
-  .then(deletedDay=>{
-  })
-  .catch(err=>{
-    console.log(err,"<--err")
-  })
+  const removeDay = (e) => {
+    axios.delete(`${import.meta.env.VITE_BACKEND_URL}/days/all/${props.dayObj._id}`)
+      .then(deletedDay => {
+      })
+      .catch(err => {
+        console.log(err, "<--err")
+      })
   }
-    return (
-          <div className='day-card'>
-            {!props.dayObj&&(
-              <>
-              </>
-            )}
-            {props.dayObj&&(
-             <> 
-            <div id="button-box">
+  return (
+    <div className='day-card'>
+      {!props.dayObj && (
+        <>
+        </>
+      )}
+      {props.dayObj && (
+        <>
+          <div id="button-box">
             <form onSubmit={removeDay}>
-            <button>×</button>
+              <button>×</button>
             </form>
-            <h1 style={{textAlign:'center'}}>{(new Date(dateTag)).toDateString()}</h1>  
-            </div>
-            {props.dayObj.myEvents.length>=1?<button onClick={editDate}>change</button>:<></>}
-            {isEditing?(<>
+            <h1 style={{ textAlign: 'center' }}>{(new Date(dateTag)).toDateString()}</h1>
+          </div>
+          {props.dayObj.myEvents.length >= 1 ? <button onClick={editDate}>change</button> : <></>}
+          {isEditing ? (<>
             <div className='date-edit'>
-            <input type="date" onChange={handleDayInputState}/>
-            <button onClick={submitEditedDate}>✅</button>
+              <input type="date" onChange={handleDayInputState} />
+              <button onClick={submitEditedDate}>✅</button>
             </div>
-            </>):(<>
-            </>)}
-             </>
-              )}
-            <button onClick={()=>{
-              //we are passing the 'dayId' here in this parameter
-              props.addEvent(props.dayObj._id)}}>＋</button>
-            {
-              props.dayObj.myEvents.map((event)=>{
-              return(    
-              <>
-              <Evententry event={event}/>
-              </>
-              )
-            })
-            }
-            {!props.dayObj.myEvents.length&&(
-              <>
-              </>
-            )}
-        </div>
-    );
+          </>) : (<>
+          </>)}
+        </>
+      )}
+      <button onClick={() => {
+        //we are passing the 'dayId' here in this parameter
+        props.addEvent(props.dayObj._id)
+      }}>＋</button>
+      {
+        props.dayObj.myEvents.map((event) => {
+          return (
+            <>
+              <Evententry event={event} />
+            </>
+          )
+        })
+      }
+      {!props.dayObj.myEvents.length && (
+        <>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Day;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Day from '../components/day';
-import { useContext,useState } from 'react';
+import { useContext, useState } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../context/auth.context';
 
 const Home = ({ allDays, setAllDays }) => {
-    const {user,isLoggedIn, logOut} = useContext(AuthContext)
-    
-    const addDay=()=>{
+    const { user, isLoggedIn, logOut } = useContext(AuthContext)
+
+    const addDay = () => {
         //until a date is in the db, there is no ._id to associate the new event with resulting in a bug when making new events
         //on a new day object 
-    setAllDays([...allDays, { day: new Date(), quote: '', myEvents: []}])
+        setAllDays([...allDays, { day: new Date(), quote: '', myEvents: [] }])
     }
     const addEvent = (dayId) => {
-    
-    const mappedDays=allDays.map(day=>{
-            if(day._id===dayId){
-                day.myEvents.push({userEntry:"",dayId})
+
+        const mappedDays = allDays.map(day => {
+            if (day._id === dayId) {
+                day.myEvents.push({ userEntry: "", dayId })
             }
             return day
         })
@@ -24,21 +24,21 @@ const Home = ({ allDays, setAllDays }) => {
     }
     return (
         <div className='home'>
-           {isLoggedIn?<button id='add-event-button' onClick={addDay}>＋</button>:<></>}
-           {!isLoggedIn?<>
-<h2>log in to begin adding tasks</h2>
-           </>:<>
-           </>}
+            {isLoggedIn ? <button id='add-event-button' onClick={addDay}>＋</button> : <></>}
+            {!isLoggedIn ? <>
+                <h2>log in to begin adding tasks</h2>
+            </> : <>
+            </>}
             <div className='days'>
-            
-            {
-            allDays.map((day)=>{
-                return (
-                <Day dayObj={day} addEvent={addEvent} />
-            )
-            })
-         
-            }
+
+                {
+                    allDays.map((day) => {
+                        return (
+                            <Day dayObj={day} addEvent={addEvent} allDays={allDays} setAllDays={setAllDays} />
+                        )
+                    })
+
+                }
             </div>
         </div>
     );
